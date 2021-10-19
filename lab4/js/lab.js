901e9f85e3f34f9eed5c1c6c91cfa96336b0849d
Zhiheng Su <zsu44@ucsc.edu>
2021-10-18 23:50:52 -0700

    lab 5
    adding js file and variable into the lab.
    adding cars as example

---
+++ lab4/js/lab.js
@@ -0,0 +1,37 @@
+// Summary: This lab is working on pseudocode for daily task and computer task.
+// Description: Creating a pseudocode on doing laundry everyday and creating an Excel form for student's test score.
+// @link   URL
+// @file   This file defines lab4/index.html.
+// @author Zhiheng Su.
+// @since  10/13/2021
+
+
+// Doing laundry:
+// Gather clothes
+  // if sort = True, sort clothes by lights and darks
+  // if prewash = True, wash stains on clothes by hand with soap and water
+  // Put clothes in washing machine
+    // Add detergent to washing machine
+      // if softener = True, add fabric softener
+        // Close all machine doors and components
+          // Adjust settings for washing machine (time, load size, soil level, etc.)
+            // Start washing machine and wait
+              // Return when finished and move wet clothes into dryer
+                // if lint = True, clean lint tray
+                  // if dryersheet = True, add dryer sheets into dryer with clothes
+                    // Close all machine doors and components
+                      // Adjust settings for dryer (time, load size, temperature, etc.)
+                        // Start dryer and wait
+                          // Return when finished and collect dry clothes
+
+// Creating an Excel form for score:
+// Creating the Excel spreadsheet
+  // Choose what type of data is going to be put in the spreadsheet, in this case will be student’s test score
+    // Create rows and columns
+      // Insert all the data in
+        // Name
+        // Student ID
+        // Scores
+          // Calculate the highest, lowest and the average score
+            // Input everything down the bottom
+              // Save it and close the sheet
---
+++ lab5/js/lab5.js
@@ -0,0 +1,20 @@
+/**
+ * Author:    Zhiheng Su <zsu44@ucsc.edu>
+ * Created:   18.10.2021
+ * License: Public Domain
+ **/
+
+ // Define Variables
+var make = "Toyota";
+var model = "Sienna";
+var color = "Grey";
+var year = 2005;
+// calculate
+var age = 2021-year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
